f14f0ae5835b6d76e38c76a3536b52920a880637
gabrielgili-jpg <gabrielgili@alunos.utfpr.edu.br>
2025-08-20 10:08:05 -0300

    Update exercicio3_.c

--- exercicio3_.c
+++ exercicio3_.c
@@ -1,23 +1,20 @@
 #include <stdio.h>
 
 int main() {
-    int i;
-    float f;
-    char c;
+	int i;
+	float f;
+	char c;
 
-    printf("Digite um caractere: ");
-    scanf(" %c", &c);
+	printf("Digite um caractere: ");
+	scanf(" %c", &c);
+	printf("Digite um valor inteiro: ");
+	scanf("%d", &i);
+	printf("Entre com um numero de ponto flutuante (valor nao inteiro): ");
+	scanf("%f", &f);
+	printf("VocC* digitou:\n");
+	printf("Caractere: %c\n", c);
+	printf("Inteiro: %d\n", i);
+	printf("NC:mero de ponto flutuante: %.2f\n", f);
 
-    printf("Digite um valor inteiro: ");
-    scanf("%d", &i);
-
-    printf("Entre com um numero de ponto flutuante (valor nao inteiro): ");
-    scanf("%f", &f);
-
-    printf("Você digitou:\n");
-    printf("Caractere: %c\n", c);
-    printf("Inteiro: %d\n", i);
-    printf("Número de ponto flutuante: %.2f\n", f);
-
-    return 0;
+	return 0;
 }
