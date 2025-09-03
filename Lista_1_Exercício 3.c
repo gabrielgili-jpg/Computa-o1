02d14c40a3db67ed2f87875644f41731ce02730b
gabrielgili-jpg <gabrielgili@alunos.utfpr.edu.br>
2025-09-03 11:13:43 -0300

    Update Lista_1_Exercício 3.c

--- Lista_1_Exercício 3.c
+++ Lista_1_Exercício 3.c
@@ -9,14 +9,14 @@ int main() {
 	scanf("%d", &n);
 
 	printf("Digite os numeros reais: \n");
-	 for(int i = 0; i < n; i++) {
-	    
-	    
-	scanf("%lf %lf %lf", &v1, &v2, &v3);
+	for(int i = 0; i < n; i++) {
+
+
+		scanf("%lf %lf %lf", &v1, &v2, &v3);
 		media_ponderada = (v1 * 2 + v2 * 3 + v3 * 5) / 10.0;
-		
-		
-	printf("A media ponderada: %.1lf\n", media_ponderada);
+
+
+		printf("A media ponderada: %.1lf\n", media_ponderada);
 	}
 
 	return 0;
